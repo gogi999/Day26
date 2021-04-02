aa47332e3ccd64585d1386cac361ce590056978f
gogi999 <g.petrovic9@gmail>
2021-04-02 14:08:30 +0200

    Updated Day26/ex08/src/PostRequestSetHeaders.js

--- ex08/src/PostRequestSetHeaders.js
+++ ex08/src/PostRequestSetHeaders.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 
-class PostRequestSetHeaders extends Component {
+class PostRequestSetHeaders extends React.Component {
     constructor(props) {
         super(props);
         this.state = { articleId: null };
