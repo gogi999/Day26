5ba3a4099d2ec805453b348f423edd5fd946c032
gogi999 <g.petrovic9@gmail>
2021-04-02 14:00:34 +0200

    Updated Day26/ex01/src/GetRequest.js

--- ex01/src/GetRequest.js
+++ ex01/src/GetRequest.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import axios from 'axios';
 
-class GetRequest extends Component {
+class GetRequest extends React.Component {
     constructor(props) {
         super(props);
         this.state = { totalReactPackages: null };
