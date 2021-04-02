50a537c749b964f85396ce8b13f2269afefbb863
gogi999 <g.petrovic9@gmail>
2021-04-02 13:56:51 +0200

    Updated Day26/ex03/src/GetRequestErrorHandling.js

--- ex03/src/GetRequestErrorHandling.js
+++ ex03/src/GetRequestErrorHandling.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import axios from 'axios';
 
-class GetRequestErrorHandling extends Component {
+class GetRequestErrorHandling extends React.Component {
     constructor(props) {
         super(props);
         this.state = { totalReactPackages: null, errorMessage: null };
